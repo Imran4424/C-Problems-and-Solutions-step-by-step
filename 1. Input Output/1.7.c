f5e9ad33f0be7b3e84c8b34c817abbc2ecb5eaa0
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-09-05 09:13:03 +0600

    c step by step

--- 1. Input Output/1.7.c
+++ 1. Input Output/1.7.c
@@ -20,3 +20,21 @@ int main()
 
 	return 0;
 }
+
+/*
+	here %lf is the format specifier of double number
+
+	by default printf() print the double number with 6 precision floating point.
+
+	but we can print our desired precision using %.<int number>f format 
+
+	Example:
+
+			%.1f - 1 precision
+			%.2f - 2 precision
+			%.3f - 3 precision
+			%.4f - 4 precision
+			%.5f - 5 precision
+
+			and so on
+*/
