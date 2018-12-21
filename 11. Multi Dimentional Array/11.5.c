5db94b88d4cd082b2f730ba0440e9f780c0564bc
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-21 09:46:35 +0600

    loop

--- 11. Multi Dimentional Array/11.5.c
+++ 11. Multi Dimentional Array/11.5.c
@@ -11,6 +11,49 @@ int main(int argc, char const *argv[])
 	int a[a_row][a_col], b[b_row][b_col], c[a_row][b_col];
 
 
+	for (int i = 0; i < a_row; ++i)
+	{
+		for (int j = 0; j < a_col; ++j)
+		{
+			scanf("%d", &a[a_row][a_col]);
+		}
+	}
+
+	for (int i = 0; i < b_row; ++i)
+	{
+		for (int j = 0; j < b_col; ++j)
+		{
+			scanf("%d", &b[b_row][b_col]);
+		}
+	}
+
+
+
+	// Multipication
+
+	for (int i = 0; i < a_row; ++i)
+	{
+		for (int j = 0; j < b_col; ++j)
+		{
+			c[i][j] = 0
+
+			for (int k = 0; k < b_row; ++k)
+			{
+				c[i][j] = c[i][j] + a[i][k] * b[k][j];
+			}
+		}
+	}
+
+
+	// Display
+
+	for (int i = 0; i < b_row; ++i)
+	{
+		for (int j = 0; j < b_col; ++j)
+		{
+			printf("%d ", c[i][j]);
+		}
+	}
 
 	return 0;
 }
