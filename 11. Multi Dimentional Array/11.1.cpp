f98ad9abbb71b6a2ed4e3bf456467a42e9f51899
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-21 08:35:25 +0600

    loop

--- 11. Multi Dimentional Array/11.1.cpp
+++ 11. Multi Dimentional Array/11.1.cpp
@@ -3,8 +3,30 @@
 
 int main(int argc, char const *argv[])
 {
-	
+	int row, col;
 
+	printf("Input the 2D array size in row and column manner\n");
+
+	printf("row: ");
+	scanf("%d", &row);
+
+	printf("col: ");
+	scanf("%d", &row);
+
+	int ar[row][col];
+
+	printf("Now give the 2D array input\n");
+
+
+	for(int i = 0; i < row ; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			printf("ar[i][j]: \n");
+			scanf("%d", &ar[i][j]);
+			printf("\n");
+		}
+	}
 
 	return 0;
 }
