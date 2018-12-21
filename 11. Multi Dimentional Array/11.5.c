fb79717c727353c1ef269885d0b25ce796f38b1e
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-21 09:34:00 +0600

    loop

--- 11. Multi Dimentional Array/11.5.c
+++ 11. Multi Dimentional Array/11.5.c
@@ -5,6 +5,9 @@ int main(int argc, char const *argv[])
 {
 	int a_row, a_col, b_row, b_col;
 
+	scanf("%d %d", &a_row, &a_col);
+	scanf("%d %d", &b_row, &b_col);
+
 	int a[a_row][a_col], b[b_row][b_col], c[a_row][b_col];
 
 
