3c9882997bb3d687c596b910471a92a43b85bdee
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2021-03-01 20:43:54 +0600

    math.h

--- 2. Math.h/3.1.c
+++ 2. Math.h/3.1.c
@@ -1,3 +1,19 @@
 /**
 	Write a program that read any integer number and display absolute value
 */
+
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int number;
+	
+	printf("Enter an integer value\n");
+	scanf("%d", &number);
+
+	printf("absolute value is %d\n", abs(number));
+
+	return 0;
+}
